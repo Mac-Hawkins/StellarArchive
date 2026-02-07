87e505555518b750308505839417594795d0ecd8
Mac-Hawkins <mckenziejhawkins8@gmail.com>
2026-02-07 12:58:56 -0500

    The APOD image and title is now successfully being displayed. Fixed typo and added comments in .env.example.

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,20 @@
-import { useEffect } from "react";
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Image, Text, View } from "react-native";
 
 
 // useEffect is a hook that allows us to fetch data.
 // useState is a hook that allows us to manage state in a state variable that we can then display.
+
+// Interface to represent the Astronomy Picture of the Day (APOD) data structure returned by the NASA API.
+interface Apod {
+  date: string;
+  explanation: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+}
+
 
 // Retrieve NASA_API_KEY from .env file.
 const API_KEY = process.env.EXPO_PUBLIC_NASA_API_KEY;
@@ -11,13 +22,16 @@ const API_KEY = process.env.EXPO_PUBLIC_NASA_API_KEY;
 // Entry point of application. This is the first screen that users see when they open the app.
 export default function Index() {
 
+  // apods being the APOD itself, and setApod being the function to update the state variable APOD.
+  const [apod, setApod] = useState<Apod>(); // State variable to store the fetched APOD.
+
   // Hooks from react to fetch data.
   useEffect(() => {
-    // Fetch astronyomy images from NASA API when the component mounts.
-    fetchAstroImgs();
+    // Fetch astronomy images from NASA API when the component mounts.
+    fetchApods();
   }, []);
 
-  async function fetchAstroImgs() {
+  async function fetchApods() {
     try {
       
       const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
@@ -27,6 +41,9 @@ export default function Index() {
 
       // Debug log for tesing purposes.
       console.log(data);
+
+      // Update the state variable with the fetched APOD data.
+      setApod(data); 
     }
     catch (error) {
       console.error("Error fetching data:", error);
@@ -41,7 +58,16 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello World! This is a page for AstroMobileApp.</Text>
+      {/* The title of the APOD image */}
+      <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 20 }}>
+        {apod?.title}
+      </Text>
+
+      
+      {/* The APOD image itself */}
+      <Image 
+      style={{ width: 300, height: 300 }}
+      source={{ uri: apod?.url }}/>
     </View>
   );
 }
