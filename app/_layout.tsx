d24a054f18dd1213870b4839606987a31618f1ac
cyberHawk8 <155265875+cyberHawk8@users.noreply.github.com>
2026-02-08 10:13:27 -0500

    Auto Formatted.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,27 +3,33 @@ import { Stack } from "expo-router";
 // "expo-router" allows us to navigate between screens.
 
 export default function RootLayout() {
+  // Stack of screens in the app.
+  return (
+    <Stack
+      screenOptions={{
+        gestureEnabled: true, // Enable swipe gestures
+      }}
+    >
+      {/* names need to match name of file. */}
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: "Astronomy Picture of the Day",
+          headerTitleAlign: "center", // This centers the title
+          headerStyle: { backgroundColor: "#acacc7" },
+        }}
+      />
 
-   // Stack of screens in the app.
-  return <Stack
-  screenOptions={{
-      gestureEnabled: true // Enable swipe gestures
-  }}>
-    {/* names need to match name of file. */}
-    <Stack.Screen 
-    name="index" 
-    options={{ 
-      headerTitle: "Astronomy Picture of the Day", 
-      headerTitleAlign: "center", // This centers the title
-      headerStyle: { backgroundColor: "#acacc7" },
-      }} />
-
-    {/* headerBackButtonDisplayMode:minimal  */}
-    <Stack.Screen name="apod_explanation" options={{ 
-      headerTitle: "Explanation", 
-      headerTitleAlign: "center", // This centers the title
-      headerBackButtonDisplayMode: "minimal", // Removes text next to back arrow.
-      headerStyle: { backgroundColor: "#6679cc" } 
-      }} />
-  </Stack>;
+      {/* headerBackButtonDisplayMode:minimal  */}
+      <Stack.Screen
+        name="apod_explanation"
+        options={{
+          headerTitle: "Explanation",
+          headerTitleAlign: "center", // This centers the title
+          headerBackButtonDisplayMode: "minimal", // Removes text next to back arrow.
+          headerStyle: { backgroundColor: "#6679cc" },
+        }}
+      />
+    </Stack>
+  );
 }
--- app/apod_explanation.tsx
+++ app/apod_explanation.tsx
@@ -1,14 +1,12 @@
 import { useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
 
-
 // This is the screen that appears when users click on an APOD image in the main app.
 export default function ApodExplanation() {
+  // Hook to access the search parameters from the URL, which includes the date of the APOD.
+  const params = useLocalSearchParams();
 
-    // Hook to access the search parameters from the URL, which includes the date of the APOD.
-    const params = useLocalSearchParams(); 
-
-    console.log(params);
+  console.log(params);
 
   return (
     <View
@@ -18,12 +16,8 @@ export default function ApodExplanation() {
         alignItems: "center",
       }}
     >
-
-    {/* Display the explanation of the APOD, which is passed as a parameter. */}
-    <Text style={{ fontSize: 18, margin: 20 }}>
-        {params.explanation}
-    </Text>
-        
+      {/* Display the explanation of the APOD, which is passed as a parameter. */}
+      <Text style={{ fontSize: 18, margin: 20 }}>{params.explanation}</Text>
     </View>
   );
 }
