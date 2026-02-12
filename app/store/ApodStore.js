2551216a11381b5b3f77eb6e246801ec513cadb8
cyberHawk8 <155265875+cyberHawk8@users.noreply.github.com>
2026-02-11 20:33:32 -0500

    Cached past API queries to prevent new queries, which allowed removal of new screens being added per APOD.

--- app/store/ApodStore.js
+++ app/store/ApodStore.js
@@ -3,6 +3,9 @@ import { create } from "zustand";
 // Create a Zustand store to manage the state of the APOD data and the bottom sheet.
 // I need this as a way to allow the bottomSheet to be re-opened after it has been closed, without having to refresh the page or navigate away and back.
 export const useApodStore = create((set) => ({
+  // Dictionary to store past APOD queries, with the date as the key and the APOD data as the value.
+  // This allows us to cache previously fetched APODs and avoid unnecessary API calls when the user swipes back to a previously viewed date.
+  pastQueries: {},
   // The state
   isSheetOpen: false,
 
